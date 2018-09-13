ec2a00818ac634c27fa2711f4053cd7e461f014d
naphisaporn <pewdog30@hotmail.com>
2018-09-13 10:27:22 +0700

    charactercard2

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CharacterCard from './CharacterCard';
+const word = "Hello";
+
 
 class App extends Component {
   render() {
     
     return (
       <div className="App">
-        <CharacterCard value="h" />
-        <CharacterCard value="i" />
+      {
+        Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+      }
       </div>
     );
   }
