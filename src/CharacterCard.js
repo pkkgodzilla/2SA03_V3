a067a585fae8e9122faf1377e531307c739249a1
naphisaporn <pewdog30@hotmail.com>
2018-09-13 11:54:03 +0700

    check3

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -3,23 +3,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 export default class CharacterCard extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
-        active: false,
+            active: false,
         }
+    }
+
+    activate = () => {
+        if (!this.state.active) {
+            this.setState({ active: true})
         }
-       
-        activate = () => {
-        if(!this.state.active){
-        this.setState({active: true, seq})
-        }
-        }
-       
+    }
     render() {
-        let className = `card ${this.state.active ? 'activeCard': ''}`
+        let className = `card ${this.state.active ? 'activeCard' : ''}`
         return (
-            <div className = "card" onClick={this.activate}> {this.props.value}</div>
+            <div className={className} onClick={this.activate}>
+                {this.props.value}
+            </div>
         )
     }
 }
